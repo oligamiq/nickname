fc7321746ed208c87b77c448060a16424237ced2
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-11-19 19:08:41 +0900

    patch

--- workflow/android/run.sh
+++ workflow/android/run.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 cargo apk build
+
 nohup cargo apk run > /dev/null 2>&1 &
+
+PID=$!
+
 adb logcat RustStdoutStderr:D '*:S' | while IFS= read -r line; do
     echo "$line"
     if [[ "$line" == *"__finish__"* ]]; then
         echo "String '__finish__' found. Killing the process."
-        # 親シェルの子プロセスで実行されるため、このスクリプト自体のPIDではなく、
-        # 直前に起動されたcargoのPIDを取得して終了します。
-        pid=$(pgrep -o "cargo")
-        if [ -n "$pid" ]; then
-            kill "$pid"
-        else
-            echo "Error: 'cargo' process not found."
-        fi
+        kill $PID
         break
     fi
 done
