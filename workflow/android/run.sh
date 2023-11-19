39aaa49eec5eb4c34d9afac32d0c4f6d5d110540
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-11-19 18:15:32 +0900

    patch

--- workflow/android/run.sh
+++ workflow/android/run.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 cargo apk run | while IFS= read -r line; do
     echo "$line"
-    if [[ "$line" == *"♰finish♰"* ]]; then
+    if [[ "$line" == *"finish"* ]]; then
         echo "String 'finish' found. Killing the process."
         pkill -f "cargo apk run"
     fi
