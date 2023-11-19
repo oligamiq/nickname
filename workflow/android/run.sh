db589c323eaac316216ad6a8342f2c3f3842d6bf
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-11-19 18:16:34 +0900

    patch

--- workflow/android/run.sh
+++ workflow/android/run.sh
@@ -4,6 +4,14 @@ cargo apk run | while IFS= read -r line; do
     echo "$line"
     if [[ "$line" == *"finish"* ]]; then
         echo "String 'finish' found. Killing the process."
-        pkill -f "cargo apk run"
+        # 親シェルの子プロセスで実行されるため、このスクリプト自体のPIDではなく、
+        # 直前に起動されたcargoのPIDを取得して終了します。
+        pid=$(pgrep -o "cargo")
+        if [ -n "$pid" ]; then
+            kill "$pid"
+        else
+            echo "Error: 'cargo' process not found."
+        fi
+        break
     fi
 done
