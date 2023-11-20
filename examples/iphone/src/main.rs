7bdc4d63ea2354caad14780e3f2b6450ee8489c8
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-11-20 10:45:20 +0900

    integrate ci_pc

--- examples/iphone/src/main.rs
+++ examples/iphone/src/main.rs
@@ -5,8 +5,7 @@ use cacao::uikit::{
     WindowSceneDelegate,
 };
 
-use cacao::color::Color;
-use cacao::layout::{Layout, LayoutConstraint};
+use cacao::layout::LayoutConstraint;
 use cacao::view::{View, ViewController, ViewDelegate};
 
 #[derive(Default)]
@@ -38,40 +37,8 @@ impl Default for RootView {
 impl ViewDelegate for RootView {
     const NAME: &'static str = "RootView";
 
-    fn did_load(&mut self, view: View) {
-        self.green.set_background_color(Color::SystemGreen);
-        view.add_subview(&self.green);
-
-        self.blue.set_background_color(Color::SystemBlue);
-        view.add_subview(&self.blue);
-
-        LayoutConstraint::activate(&[
-            self.green
-                .leading
-                .constraint_equal_to(&view.leading)
-                .offset(16.),
-            self.green
-                .trailing
-                .constraint_equal_to(&view.trailing)
-                .offset(-16.),
-            self.green.height.constraint_equal_to_constant(120.),
-            self.blue
-                .top
-                .constraint_equal_to(&self.green.bottom)
-                .offset(16.),
-            self.blue
-                .leading
-                .constraint_equal_to(&view.leading)
-                .offset(16.),
-            self.blue
-                .trailing
-                .constraint_equal_to(&view.trailing)
-                .offset(-16.),
-            self.blue
-                .bottom
-                .constraint_equal_to(&view.bottom)
-                .offset(-16.),
-        ]);
+    fn did_load(&mut self, _view: View) {
+        LayoutConstraint::activate(&[]);
     }
 }
 
@@ -82,7 +49,7 @@ pub struct WindowScene {
 }
 
 impl WindowSceneDelegate for WindowScene {
-    fn will_connect(&self, scene: Scene, session: SceneSession, options: SceneConnectionOptions) {
+    fn will_connect(&self, scene: Scene, _session: SceneSession, _options: SceneConnectionOptions) {
         let bounds = scene.get_bounds();
         let mut window = Window::new(bounds);
         window.set_window_scene(scene);
