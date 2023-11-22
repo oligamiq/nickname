d32cf1ae44e4da2c9596c31cd105e4df0e3c8e16
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-11-22 18:01:58 +0900

    patch

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() {
     #[cfg(target_os = "ios")]
-    println!("cargo:rustc-link-lib=framework=UIKit");
+    {
+        println!("cargo:rustc-link-lib=framework=UIKit");
+
+        println!("cargo:rustc-link-lib=framework=CoreGraphics");
+        println!("cargo:rustc-link-lib=framework=QuartzCore");
+        println!("cargo:rustc-link-lib=framework=Security");
+    }
 }
