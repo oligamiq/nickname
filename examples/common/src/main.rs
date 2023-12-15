9f255a5647a3d6a760b37b9cfbf6508fd762d73b
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-12-15 14:22:16 +0900

    fix main

--- examples/common/src/main.rs
+++ examples/common/src/main.rs
@@ -3,12 +3,12 @@ fn main() {
     let device_name = nickname.get().unwrap();
     println!("{device_name}");
 
-    // #[cfg(not(any(target_os = "ios", target_os = "android")))]
-    // {
-    //     let set = nickname.set("oligami-pc");
-    //     println!("{:?}", set);
+    #[cfg(not(any(target_os = "ios", target_os = "android")))]
+    {
+        let set = nickname.set("oligami-pc");
+        println!("{:?}", set);
 
-    //     let device_name = nickname.get().unwrap();
-    //     println!("{device_name}");
-    // }
+        let device_name = nickname.get().unwrap();
+        println!("{device_name}");
+    }
 }
